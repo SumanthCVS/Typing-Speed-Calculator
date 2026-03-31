8e41521c7823431fb399cc3fe7bc358bb47a15a3
Sumanth CVS <cvsumanth8888@gmail.com>
2026-03-31 23:17:02 +0530

    changed wpm calc formula and others

--- 20wordtest.c
+++ 20wordtest.c
@@ -5,166 +5,193 @@
 
 #define MAX_TEXT 2000
 
-void clearScreen() {
-    #ifdef _WIN32
-        system("cls");
-    #else
-        system("clear");
-    #endif
+void clearScreen()
+{
+#ifdef _WIN32
+    system("cls");
+#else
+    system("clear");
+#endif
 }
 
-int main() {
-    // Simple, common English words for typing practice
-    char *word_bank[] = {
-        "the", "and", "have", "that", "for", "you", "with", "say", "this", "they",
-        "but", "his", "from", "not", "she", "what", "their", "can", "who", "get",
-        "would", "her", "make", "about", "know", "will", "one", "time", "there", "year",
-        "think", "when", "which", "them", "some", "people", "take", "out", "into", "just",
-        "see", "him", "your", "come", "could", "now", "than", "like", "other", "how",
-        "then", "its", "our", "two", "more", "these", "want", "way", "look", "first",
-        "also", "new", "because", "day", "use", "no", "man", "find", "here", "thing",
-        "give", "many", "well", "only", "those", "tell", "very", "even", "back", "any",
-        "good", "woman", "through", "us", "life", "child", "work", "down", "may", "after",
-        "should", "call", "world", "over", "school", "still", "try", "in", "as", "on",
-        "is", "it", "at", "by", "an", "be", "if", "or", "do", "so",
-        "up", "long", "great", "where", "little", "round", "show", "every", "me", "most",
-        "much", "before", "line", "right", "too", "means", "old", "same", "boy", "follow",
-        "three", "small", "set", "put", "end", "does", "another", "large", "must", "big",
-        "even", "such", "turn", "ask", "went", "men", "read", "need", "land", "different",
-        "home", "move", "kind", "hand", "picture", "again", "change", "off", "play", "spell",
-        "air", "away", "animal", "house", "point", "page", "letter", "mother", "answer", "found",
-        "study", "learn", "America", "high", "near", "add", "food", "between", "own", "below",
-        "country", "plant", "last", "father", "keep", "tree", "never", "start", "city", "earth",
-        "eye", "light", "thought", "head", "under", "story", "saw", "left", "few", "while",
-        "along", "might", "close", "something", "seem", "next", "hard", "open", "example", "begin",
-        "always", "both", "paper", "together", "got", "group", "often", "run", "important", "until",
-        "children", "side", "feet", "car", "mile", "night", "walk", "white", "sea", "began",
-        "grow", "took", "river", "four", "carry", "state", "once", "book", "hear", "stop",
-        "without", "second", "later", "miss", "idea", "enough", "eat", "face", "watch", "far",
-        "Indian", "really", "almost", "let", "above", "girl", "sometimes", "mountain", "cut", "young",
-        "talk", "soon", "list", "song", "being", "leave", "family", "body", "music", "color",
-        "stand", "sun", "questions", "fish", "area", "mark", "dog", "horse", "birds", "problem",
-        "complete", "room", "knew", "since", "ever", "piece", "told", "usually", "friends", "easy",
-        "heard", "order", "red", "door", "sure", "become", "top", "ship", "across", "today",
-        "during", "short", "better", "best", "however", "low", "hours", "black", "products", "happened",
-        "whole", "measure", "remember", "early", "waves", "reached", "listen", "wind", "rock", "space",
-        "covered", "fast", "several", "hold", "toward", "five", "step", "morning", "passed", "vowel",
-        "true", "hundred", "against", "pattern", "numeral", "table", "north", "slowly", "money", "map",
-        "farm", "pulled", "draw", "voice", "seen", "cold", "cried", "plan", "notice", "south",
-        "sing", "war", "ground", "fall", "king", "town", "unit", "figure", "certain", "field",
-        "travel", "wood", "fire", "upon", "done", "English", "road", "halt", "ten", "fly",
-        "gave", "box", "finally", "wait", "correct"
+int main()
+{
+    char *word_bank[]={
+        "the","and","have","that","for","you","with","say","this","they",
+        "but","his","from","not","she","what","their","can","who","get",
+        "would","her","make","about","know","will","one","time","there","year",
+        "think","when","which","them","some","people","take","out","into","just",
+        "see","him","your","come","could","now","than","like","other","how",
+        "then","its","our","two","more","these","want","way","look","first",
+        "also","new","because","day","use","no","man","find","here","thing"
     };
-    int word_bank_size = sizeof(word_bank) / sizeof(word_bank[0]);
-    char random_sentence[2000] = "";
-    int num_words = 20;
-    int unique_indices[30]; // support up to 30
-    int count = 0;
-    srand((unsigned int)time(NULL));
-    // Generate num_words unique random indices
-    while (count < num_words) {
-        int idx = rand() % word_bank_size;
-        int duplicate = 0;
-        for (int j = 0; j < count; j++) {
-            if (unique_indices[j] == idx) {
-                duplicate = 1;
+
+    int word_bank_size=
+    sizeof(word_bank)/sizeof(word_bank[0]);
+
+    char random_sentence[MAX_TEXT]="";
+
+    int num_words=20;
+
+    int unique_indices[30];
+
+    int count=0;
+
+
+    srand(time(NULL));
+
+    while(count<num_words)
+    {
+        int idx=rand()%word_bank_size;
+
+        int duplicate=0;
+
+        for(int j=0;j<count;j++)
+        {
+            if(unique_indices[j]==idx)
+            {
+                duplicate=1;
                 break;
             }
         }
-        if (!duplicate) {
-            unique_indices[count++] = idx;
-        }
+
+        if(!duplicate)
+        unique_indices[count++]=idx;
     }
-    for (int i = 0; i < num_words; i++) {
-        strcat(random_sentence, word_bank[unique_indices[i]]);
-        if (i < num_words - 1) strcat(random_sentence, " ");
+
+    for(int i=0;i<num_words;i++)
+    {
+        strncat(random_sentence,
+        word_bank[unique_indices[i]],
+        MAX_TEXT-strlen(random_sentence)-1);
+
+        if(i<num_words-1)
+        strncat(random_sentence," ",
+        MAX_TEXT-strlen(random_sentence)-1);
     }
 
     char input[MAX_TEXT];
-    // clearScreen(); // Removed to keep the sentence visible
+
+    clearScreen();
+
     printf("===== Typing Speed Test =====\n\n");
-    printf("Type the following sentence as fast as you can:\n\n");
-    // Print the sentence, wrapping only between words (not in the middle of a word)
-    int line_len = 0;
-    int max_line = 60; // characters per line before wrapping
-    for (int i = 0; i < num_words; i++) {
-        int word_len = strlen(word_bank[unique_indices[i]]);
-        if (line_len + word_len > max_line) {
+
+    printf("Type the following sentence:\n\n");
+
+    int line_len=0;
+
+    int max_line=60;
+
+    for(int i=0;i<num_words;i++)
+    {
+        int word_len=
+        strlen(word_bank[unique_indices[i]]);
+
+        if(line_len+word_len>max_line)
+        {
             printf("\n");
-            line_len = 0;
+            line_len=0;
         }
-        printf("%s", word_bank[unique_indices[i]]);
-        line_len += word_len;
-        if (i < num_words - 1) {
+
+        printf("%s",
+        word_bank[unique_indices[i]]);
+
+        line_len+=word_len;
+
+        if(i<num_words-1)
+        {
             printf(" ");
             line_len++;
         }
     }
-    printf("\n\n");
-    printf("Press ENTER when ready...");
+
+    printf("\n\nPress ENTER when ready...");
     getchar();
 
-    // Do not clear the screen here, so the sentence remains visible
-    printf("Start typing (you have 60 seconds):\n\n");
+    printf("\nStart typing (60 sec):\n\n");
 
-    time_t start, end;
-    int timed_out = 0;
-    input[0] = '\0';
+    time_t start,end;
 
-    start = time(NULL);
-    // Read input with timer
-    for (int i = 0; i < MAX_TEXT - 1; i++) {
-        if (difftime(time(NULL), start) >= 60) {
-            timed_out = 1;
+    int timed_out=0;
+
+    int pos=0;
+
+    start=time(NULL);
+
+    while(1)
+    {
+        if(difftime(time(NULL),start)>=60)
+        {
+            timed_out=1;
             break;
         }
-        int ch = getchar();
-        if (ch == '\n' || ch == EOF) {
-            input[i] = '\0';
-            break;
+
+        int ch=getchar();
+
+        if(ch=='\n')
+        break;
+
+        if(ch==8 || ch==127)
+        {
+            if(pos>0)
+            {
+                pos--;
+                printf("\b \b");
+            }
+            continue;
         }
-        input[i] = (char)ch;
-        input[i+1] = '\0';
-    }
-    end = time(NULL);
 
-    double timeTaken = difftime(end, start) / 60.0; // in minutes
-
-    char *original = random_sentence;
-
-    // Calculate accuracy
-    int correctChars = 0;
-    int totalChars = strlen(original);
-    int inputLen = strlen(input);
-    int minLen = (inputLen < totalChars) ? inputLen : totalChars;
-    for (int i = 0; i < minLen; i++) {
-        if (input[i] == original[i]) {
-            correctChars++;
+        if(pos<MAX_TEXT-1)
+        {
+            input[pos++]=ch;
+            putchar(ch);
         }
     }
 
-    double accuracy = ((double)correctChars / totalChars) * 100;
+    input[pos]='\0';
 
-    // Calculate WPM
-    int wordCount = 0;
-    for (int i = 0; i < inputLen; i++) {
-        if (input[i] == ' ') {
-            wordCount++;
-        }
+    end=time(NULL);
+
+    double timeTaken=
+    difftime(end,start)/60.0;
+
+    if(timeTaken<=0)
+    timeTaken=0.01;
+
+    char *original=random_sentence;
+
+    int correctChars=0;
+
+    int totalChars=strlen(original);
+
+    int inputLen=strlen(input);
+
+    int minLen=
+    inputLen<totalChars?inputLen:totalChars;
+
+    for(int i=0;i<minLen;i++)
+    {
+        if(input[i]==original[i])
+        correctChars++;
     }
-    if (inputLen > 0) wordCount++; // last word
 
-    double wpm = wordCount / timeTaken;
+    double accuracy=
+    ((double)correctChars/totalChars)*100;
 
-    // Output results
-    printf("\n===== Results =====\n");
-    if (timed_out) {
-        printf("Time is up! You didn't finish in time. Give it another shot!\n");
-    } else {
-        printf("Time taken: %.2f seconds\n", difftime(end, start));
-        printf("Words per minute (WPM): %.2f\n", wpm);
-        printf("Accuracy: %.2f%%\n", accuracy);
-    }
+    double wpm=
+    ((double)inputLen/5.0)/timeTaken;
+
+    printf("\n\n===== Results =====\n");
+
+    if(timed_out)
+    printf("Time is up!\n");
+
+    printf("Time taken: %.2f sec\n",
+    difftime(end,start));
+
+    printf("WPM: %.2f\n",wpm);
+
+    printf("Accuracy: %.2f%%\n",accuracy);
 
     return 0;
 }
