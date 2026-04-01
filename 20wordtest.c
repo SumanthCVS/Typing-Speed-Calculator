221ff11550a5545aef9e45617e376bbf3a1f641e
Sumanth CVS <cvsumanth8888@gmail.com>
2026-04-01 17:30:12 +0530

    menu driven test is implemented

--- 20wordtest.c
+++ 20wordtest.c
@@ -5,14 +5,15 @@
 
 #ifdef _WIN32
 #include <conio.h>
+#include <windows.h>
+#define sleep(x) Sleep(1000*(x))
 #else
 #include <termios.h>
 #include <unistd.h>
 #include <fcntl.h>
 #endif
 
-#define MAX_TEXT 2000
-#define TEST_TIME 60
+#define MAX_TEXT 5000
 
 #ifndef _WIN32
 
@@ -20,47 +21,47 @@ struct termios oldt;
 
 void reset_terminal()
 {
-    tcsetattr(STDIN_FILENO,TCSANOW,&oldt);
+tcsetattr(STDIN_FILENO,TCSANOW,&oldt);
 }
 
 void init_terminal()
 {
-    struct termios newt;
+struct termios newt;
 
-    tcgetattr(STDIN_FILENO,&oldt);
+tcgetattr(STDIN_FILENO,&oldt);
 
-    newt=oldt;
+newt=oldt;
 
-    newt.c_lflag &= ~(ICANON | ECHO);
+newt.c_lflag &= ~(ICANON | ECHO);
 
-    tcsetattr(STDIN_FILENO,TCSANOW,&newt);
+tcsetattr(STDIN_FILENO,TCSANOW,&newt);
 
-    fcntl(STDIN_FILENO,F_SETFL,O_NONBLOCK);
+fcntl(STDIN_FILENO,F_SETFL,O_NONBLOCK);
 
-    atexit(reset_terminal);
+atexit(reset_terminal);
 }
 
 int kbhit()
 {
-    char ch;
+char ch;
 
-    if(read(STDIN_FILENO,&ch,1)==1)
-    {
-        ungetc(ch,stdin);
-        return 1;
-    }
+if(read(STDIN_FILENO,&ch,1)==1)
+{
+ungetc(ch,stdin);
+return 1;
+}
 
-    return 0;
+return 0;
 }
 
 int getch()
 {
-    char ch;
+char ch;
 
-    if(read(STDIN_FILENO,&ch,1)==1)
-    return ch;
+if(read(STDIN_FILENO,&ch,1)==1)
+return ch;
 
-    return 0;
+return 0;
 }
 
 #endif
@@ -68,20 +69,189 @@ int getch()
 void clearScreen()
 {
 #ifdef _WIN32
-    system("cls");
+system("cls");
 #else
-    system("clear");
+system("clear");
 #endif
 }
 
-int main()
+double calculateAccuracy(char *original,char *input)
+{
+int correct=0;
+
+int total=strlen(original);
+
+int inputLen=strlen(input);
+
+int minLen=inputLen<total?inputLen:total;
+
+for(int i=0;i<minLen;i++)
+{
+if(original[i]==input[i])
+correct++;
+}
+
+return ((double)correct/total)*100;
+}
+
+double calculateWPM(char *input,double timeTaken)
+{
+int wordCount=0;
+
+int len=strlen(input);
+
+for(int i=0;i<len;i++)
+{
+if(input[i]==' ')
+wordCount++;
+}
+
+if(len>0)
+wordCount++;
+
+return wordCount/timeTaken;
+}
+
+void typingTest(char *original,int TEST_TIME)
 {
 
-#ifndef _WIN32
-init_terminal();
+char input[MAX_TEXT];
+
+time_t start=0,end;
+
+int timer_started=0;
+
+int timed_out=0;
+
+int pos=0;
+
+printf("\nStart typing:\n\n");
+
+while(1)
+{
+
+if(timer_started)
+{
+if(difftime(time(NULL),start)>=TEST_TIME)
+{
+timed_out=1;
+break;
+}
+}
+
+#ifdef _WIN32
+if(_kbhit())
+{
+int ch=_getch();
+#else
+if(kbhit())
+{
+int ch=getch();
 #endif
 
+if(!timer_started)
+{
+start=time(NULL);
+timer_started=1;
+}
+
+if(ch=='\n' || ch==13)
+break;
+
+if(ch==8 || ch==127)
+{
+if(pos>0)
+{
+pos--;
+printf("\b \b");
+}
+continue;
+}
+
+if(pos<MAX_TEXT-1)
+{
+input[pos++]=ch;
+putchar(ch);
+}
+}
+}
+
+input[pos]='\0';
+
+end=time(NULL);
+
+double timeTaken=
+difftime(end,start)/60.0;
+
+if(timeTaken<=0)
+timeTaken=0.01;
+
+double accuracy=
+calculateAccuracy(original,input);
+
+double wpm=
+calculateWPM(input,timeTaken);
+
+printf("\n\n===== Results =====\n");
+
+if(timed_out)
+printf("Time up!\n");
+
+printf("Time: %.2f sec\n",
+difftime(end,start));
+
+printf("WPM: %.2f\n",wpm);
+
+printf("Accuracy: %.2f%%\n",accuracy);
+
+}
+
+void memoryTest()
+{
+
+char *sentences[]={
+"The small cat slept quietly near the warm window during the afternoon",
+"Practice a little every day and your skills will slowly become stronger",
+"Hard work with clear focus usually brings better results over time",
+"A good plan today can prevent many problems tomorrow",
+"Learning new things keeps the mind active and fresh",
+"Simple habits done daily can change your future",
+"Patience and effort together make difficult tasks easier",
+"Reading carefully helps you understand things better",
+"Step by step progress is still real progress",
+"Clear thinking helps you solve most problems"
+};
+
+int size=
+sizeof(sentences)/sizeof(sentences[0]);
+
+int idx=rand()%size;
+
+char *chosen=
+sentences[idx];
+
+clearScreen();
+
+printf("Memorize this sentence (you have 10 seconds):\n\n");
+
+printf("%s",chosen);
+
+sleep(12);
+
+clearScreen();
+
+printf("Press ENTER to start typing...");
+getchar();
+
+typingTest(chosen,45);
+
+}
+
+void randomWordTest()
+{
+
 char *word_bank[]={
+/* YOUR FULL WORD LIST HERE EXACTLY AS PROVIDED */
 "the","and","have","that","for","you","with","say","this","they",
 "but","his","from","not","she","what","their","can","who","get",
 "would","her","make","about","know","will","one","time","there","year",
@@ -122,203 +292,104 @@ char *word_bank[]={
 "gave","box","finally","wait","correct"
 };
 
-int word_bank_size=
+int size=
 sizeof(word_bank)/sizeof(word_bank[0]);
-
-char random_sentence[MAX_TEXT]="";
 
 int num_words=20;
 
-int unique_indices[30];
+int unique[500];
 
 int count=0;
 
-srand(time(NULL));
-
 while(count<num_words)
 {
-    int idx=rand()%word_bank_size;
 
-    int duplicate=0;
+int idx=rand()%size;
 
-    for(int j=0;j<count;j++)
-    {
-        if(unique_indices[j]==idx)
-        {
-            duplicate=1;
-            break;
-        }
-    }
+int dup=0;
 
-    if(!duplicate)
-    unique_indices[count++]=idx;
+for(int j=0;j<count;j++)
+{
+if(unique[j]==idx)
+{
+dup=1;
+break;
 }
+}
+
+if(!dup)
+unique[count++]=idx;
+
+}
+
+char sentence[MAX_TEXT]="";
 
 for(int i=0;i<num_words;i++)
 {
-    strcat(random_sentence,
-    word_bank[unique_indices[i]]);
 
-    if(i<num_words-1)
-    strcat(random_sentence," ");
+strcat(sentence,
+word_bank[unique[i]]);
+
+if(i<num_words-1)
+strcat(sentence," ");
+
 }
-
-char input[MAX_TEXT];
 
 clearScreen();
 
-printf("===== Typing Speed Test =====\n\n");
+printf("Type this:\n\n");
 
-printf("Type the following sentence as fast as you can:\n\n");
+printf("%s\n",sentence);
 
-int line_len=0;
-int max_line=60;
-
-for(int i=0;i<num_words;i++)
-{
-    int word_len=
-    strlen(word_bank[unique_indices[i]]);
-
-    if(line_len+word_len>max_line)
-    {
-        printf("\n");
-        line_len=0;
-    }
-
-    printf("%s",
-    word_bank[unique_indices[i]]);
-
-    line_len+=word_len;
-
-    if(i<num_words-1)
-    {
-        printf(" ");
-        line_len++;
-    }
-}
-
-printf("\n\n");
-
-printf("Press ENTER when ready...");
+printf("\nPress ENTER when ready...");
 getchar();
 
-printf("Start typing (60 seconds):\n\n");
+typingTest(sentence,60);
 
-time_t start=0,end;
+}
 
-int timed_out=0;
+int main()
+{
 
-int timer_started=0;
+#ifndef _WIN32
+init_terminal();
+#endif
 
-int pos=0;
+srand(time(NULL));
+
+int choice;
 
 while(1)
 {
-    if(timer_started)
-    {
-        if(difftime(time(NULL),start)>=60)
-        {
-            timed_out=1;
-            break;
-        }
-    }
 
-#ifdef _WIN32
-    if(_kbhit())
-    {
-        int ch=_getch();
-#else
-    if(kbhit())
-    {
-        int ch=getch();
-#endif
+printf("\nChoose one:\n");
 
-        if(!timer_started)
-        {
-            start=time(NULL);
-            timer_started=1;
-        }
+printf("1 Random word typing test\n");
 
-        if(ch=='\n' || ch==13)
-        break;
+printf("2 Memory sentence test\n");
 
-        if(ch==8 || ch==127)
-        {
-            if(pos>0)
-            {
-                pos--;
-                printf("\b \b");
-            }
-            continue;
-        }
+printf("3 Exit\n");
 
-        if(pos<MAX_TEXT-1)
-        {
-            input[pos++]=ch;
-            putchar(ch);
-        }
-    }
-}
+printf("Choice: ");
 
-input[pos]='\0';
+scanf("%d",&choice);
 
-end=time(NULL);
+getchar();
 
-double timeTaken=
-difftime(end,start)/60.0;
+if(choice==1)
+randomWordTest();
 
-if(timeTaken<=0)
-timeTaken=0.01;
+else if(choice==2)
+memoryTest();
 
-char *original=random_sentence;
+else if(choice==3)
+break;
 
-int correctChars=0;
-
-int totalChars=strlen(original);
-
-int inputLen=strlen(input);
-
-int minLen=
-(inputLen<totalChars)?inputLen:totalChars;
-
-for(int i=0;i<minLen;i++)
-{
-    if(input[i]==original[i])
-    correctChars++;
-}
-
-double accuracy=
-((double)correctChars/totalChars)*100;
-
-int wordCount=0;
-
-for(int i=0;i<inputLen;i++)
-{
-    if(input[i]==' ')
-    wordCount++;
-}
-
-if(inputLen>0)
-wordCount++;
-
-double wpm=
-wordCount/timeTaken;
-
-printf("\n===== Results =====\n");
-
-if(timed_out)
-printf("Time is up! Try again!\n");
 else
-{
-printf("Time taken: %.2f seconds\n",
-difftime(end,start));
+printf("Invalid choice\n");
 
-printf("Words per minute (WPM): %.2f\n",
-wpm);
-
-printf("Accuracy: %.2f%%\n",
-accuracy);
 }
 
 return 0;
+
 }
